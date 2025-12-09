83cc3aaec0a8bbda9a3f5b98b7534281e27acca8
myzayy <116192354+myzayy@users.noreply.github.com>
2025-12-09 21:32:50 +0100

    Added BrailleHeader and basic main logic

---
+++ braille.h
@@ -0,0 +1,20 @@
+#ifndef BRAILLE_H
+#define BRAILLE_H
+
+#include <stdint.h>
+
+// Header struct
+// Takes memory sequentially
+typedef struct {
+    char magic[4];       // 4 bytes: identifer (example., "BRL1")
+    uint8_t version;     // 1 byte: format version
+    uint8_t mode;        // 1 byte: mode (6 or 8 dotes)
+    uint8_t reserved[2]; // 2 bytes
+    uint32_t char_count; // 4 bytes
+    uint32_t checksum;
+    uint8_t padding[16]; // 16 bytes: reserve for 0-os for 32 of sum
+} BrailleHeader;
+
+//32 bytes
+
+#endif
--- main.c
+++ main.c
@@ -1,8 +1,48 @@
 #include <stdio.h>
+#include <string.h>
+#include "braille.h"
+// braille -t input.txt output.brl
 
-int main()
+
+void print_help() {
+    printf("Usage:\n");
+    printf("  Translation: program -t input.txt output.brl\n");
+    printf("  Printing:    program -p input.brl\n");
+}
+
+int main(int argc, char *argv[])
 {
 
-    printf("Hello World!\n");
+    // If there are not enough arguments, print help
+    if (argc < 3) {
+        print_help();
+        return 1;
+    }
+
+    // Check first argument (flag)
+    // strcmp compare two strings.
+    if (strcmp(argv[1], "-t") == 0) {
+        if (argc != 4) {
+            printf("Error: Invalid arguments for translation.\n");
+            return 1;
+        }
+        printf("Mode: Translation (Text -> Braille)\n");
+        printf("Input: %s\n", argv[2]);
+        printf("Output: %s\n", argv[3]);
+
+        // there will be call func translate_text()
+
+    } else if (strcmp(argv[1], "-p") == 0) {
+        printf("Mode: Printing (Braille -> Screen)\n");
+        printf("Input file: %s\n", argv[2]);
+
+        // there will be call func print_braille()
+
+    } else {
+        printf("Error: Unknown parameter %s\n", argv[1]);
+        print_help();
+        return 1;
+    }
+
     return 0;
 }
